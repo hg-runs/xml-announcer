e5f9423e88e98b04c093a5d90e0d8787c9eecd34
Gitea <gitea@fake.local>
2020-09-30 17:45:04 +0000

    Final fixes

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var parser = new xml2js.Parser({ explicitArray: false });
 var request = require("request");
 var concat = require('concat-stream');
 var fs = require("fs");
+var appRoot = require('app-root-path');
+var winston = require('winston');
+// var auth = require('./auth.json');
 
 const Discord = require('discord.js');
 
@@ -22,24 +25,49 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const token = process.env.TOKEN;
-const channelID = process.env.CHANNEL
 const interval = process.env.INTERVAL_MS
 const feedToServe = process.env.FEED
 
-const channelNWNHigherGroundsRuns = "285255258622394369"
-const channelNWNHigherGroundsTrade = "751457838345486406"
-
 const client = new Discord.Client();
 
-var logger = require('winston');
-// var auth = require('./auth.json');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-	colorize: true
+var options = {
+	file: {
+		level: 'info',
+		name: 'file.info',
+		filename: `${appRoot}/logs/app.log`,
+		handleExceptions: true,
+		json: true,
+		maxsize: 5242880, // 5MB
+		maxFiles: 100,
+		colorize: true,
+	},
+	errorFile: {
+		level: 'error',
+		name: 'file.error',
+		filename: `${appRoot}/logs/error.log`,
+		handleExceptions: true,
+		json: true,
+		maxsize: 5242880, // 5MB
+		maxFiles: 100,
+		colorize: true,
+	},
+	console: {
+		level: 'debug',
+		handleExceptions: true,
+		json: false,
+		colorize: true,
+	},
+};
+
+var logger = new winston.createLogger({
+	transports: [
+		// new (winston.transports.Console)(options.console),
+		new (winston.transports.File)(options.errorFile),
+		new (winston.transports.File)(options.file)
+	],
+	exitOnError: false, // do not exit on handled exceptions
 });
-logger.level = 'debug';
 
 var appConfig;
 var appStats = {
@@ -56,15 +84,25 @@ function jsonStringify(jstruct) {
 	return (JSON.stringify(jstruct, undefined, 4));
 }
 
-
-parser.on('error', function (err) { console.log('Parser error', err); });
+// example of a message item as seen on HD XML web page
+//ITEM:  { uid: '1463138558.706707',
+//  channel: '-4',
+//  type: 'Chat Message',
+//  senttime: '2016-05-13T13:04:27Z',
+//  server: 'Chat',
+//  sender: 'Somebody Artsy',
+//  character: 'Somebody Artsy',
+//  player: 'Lynslayer',
+//  fullmsg: 'Interserver chat message from Somebody Artsy (Lynslayer): They should add a BUR or XUR item in the game that lets you skip the  reincarnation waiting time',
+//  text: 'They should add a BUR or XUR item in the game that lets you skip the reincarnation waiting time',
+//  title: 'Chat Message: They should add a BUR or XUR item in the game that lets you skip the reincarnation waiting time, from Somebody Artsy  (Lynslayer), sent 2016-05-13 13:04:27' }
 
 function refresh_xml() {
 
 	var request = http.get(url, function (resp) {
 
 		resp.on('error', function (err) {
-			console.log('Error while reading', err);
+			logger.error('Error while reading', err);
 		});
 
 		resp.pipe(concat(function (buffer) {
@@ -72,7 +110,6 @@ function refresh_xml() {
 			var str = buffer.toString();
 			parser.parseString(str, function (err, result) {
 				logger.info('Finished parsing at ' + (new Date()).toISOString());
-				//console.log('Finished parsing:', err, result);
 
 				if (typeof result !== 'undefined') {
 
@@ -83,20 +120,16 @@ function refresh_xml() {
 							appStats.timestamps[s.name] = new Date(s.updated);
 						}
 					}
-					//console.log("   Serverinfo - timestamp last update for messages: ", appStats.timestamps["Messages"]);
 					var j;
 					// timestamp changed, check all feeds
 					for (j = 0; j < appConfig.feeds.length; ++j) {
 						if (appConfig.feeds[j].enabled) {
-							//console.log("Processing channel: " + appConfig.feeds [j].name);
 							if (!(appConfig.feeds[j].name in appStats.last_updated)) {
 								// new feed: set date of last update to 0 (start of epoch)
 								appStats.last_updated[appConfig.feeds[j].name] = new Date(0);
 								statsChanged();
 							}
 							if (new Date(appStats.timestamps["Messages"]) > new Date(appStats.last_updated[appConfig.feeds[j].name])) {
-								//console.log ("Last update: " + appStats.last_updated[appConfig.feeds [j].name]);
-								//console.log ("New update timestamp: " + appStats.timestamps["Messages"]);
 								i = appConfig.feeds[j].xmlChannelIndex;
 								scan_list_for_new_messages(result.server_data.message_list[i], new Date(appStats.last_updated[appConfig.feeds[j].name]), appConfig.feeds[j])
 								//appStats.last_updated[appConfig.feeds [j].name] = appStats.timestamps["Messages"];
@@ -106,7 +139,7 @@ function refresh_xml() {
 						}
 					}
 				} else {
-					console.log("No server data!");
+					logger.error("No server data!");
 				}
 			});
 		}));
@@ -114,28 +147,27 @@ function refresh_xml() {
 	});
 
 	request.on('error', function (err) {
-		console.log((new Date()).toISOString() + ' Error while connecting to: ' + err.address + ":" + err.port + " with code:", err.code);
+		logger.error((new Date()).toISOString() + ' Error while connecting to: ' + err.address + ":" + err.port + " with code:", err.code);
 	});
 
 
 }
 
-function send_to_discord(messagetext, messageplayer, sender, time, server, channelID, color) {
+function send_to_discord(messagetext, messageplayer, sender, time, server, channelIDs, color, showServer) {
 
 	const text = messagetext;
 	const player = messageplayer;
-	const icon = client.emojis.cache.find(emoji => emoji.name == "HGIcon_Run");
+	// const icon = client.emojis.cache.find(emoji => emoji.name == "HGIcon_Run");
 
 	// client.emojis.cache.forEach(logMapElements);
-	// console.log('Icon found: ' + icon);
-
 	// const emojiList = message.guild.emojis.map((e, x) => (x + ' = ' + e) + ' | ' +e.name).join('\n');
 	// message.channel.send(emojiList);
 
-	if (server && server != "") {
-		desc = `${player}:\n**${text}**\n*Server: ${server}*`
+	if (showServer) {
+		// desc = `${player}:\n**${text}**\n> \`\`\`yaml\n*Server: ${server}*\n\`\`\``
+		desc = `${sender}(${player}):\n**${text}**\n> \t*Server: ${server}*`
 	} else {
-		desc = `${player}:\n**${text}**`
+		desc = `${sender}(${player}):\n**${text}**`
 	}
 
 	const embed = {
@@ -158,7 +190,7 @@ function send_to_discord(messagetext, messageplayer, sender, time, server, chann
 		// "author": {
 		// 	"name": "HG XML reader",
 		// 	"url": "https://discordapp.com",
-		// 	"icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+		// 	"icon_url": "attachment://discordjs.png"
 		// },
 		// "fields": [
 		// {
@@ -173,41 +205,42 @@ function send_to_discord(messagetext, messageplayer, sender, time, server, chann
 		// 	"inline": true
 		// }
 		// ]
+		// image: {
+		// 	url: 'attachment://discordjs.png',
+		// },
 	};
 
-	var channel = client.channels.cache.get(channelID);
-	logger.info(embed);
-	channel.send({ embed });
+	// var file = new Discord.MessageAttachment('./assets/discordjs.png');
+	for (c in channelIDs) {
+		// console.log(c)
+		// console.log(channelIDs[c])
+		var channel = client.channels.cache.get(channelIDs[c]);
+		logger.info(embed);
+		channel.send({ embed });
+		// channel.send({ files: [file], embed: embed });
+	}
 }
 
 // REVIEW: Are message still not displayed in order by discord?
 function scan_list_for_new_messages(list, date_stamp, feed) {
 	var i, s, len = list.message.length;
-	//console.log('# of items: ' + list.message.length);
+	logger.info('# of items: ' + list.message.length);
 	logger.info('timestamp used: ' + date_stamp);
 	logger.info('feed: ' + feed.name);
 
 	var list_as_array = Array.prototype.slice.call(list.message);
 
-	//console.log(list);
-	//console.log(list_as_array);
 	// the list is sorted, idx 0 is latest, traversing array in reverse to send oldest as first message
 	// sort list by date, list is max 50 items, in case you need another sort order
 	list_as_array.sort(function (a, b) {
 		var c = new Date(a.senttime);
 		var d = new Date(b.senttime);
 		return d - c;
-		//return c-d;
 	});
-	//console.log(list_as_array);
-	//console.log(list_as_array[0]);
-	//console.log("Length: " + len);
 
 	// Find index of first message already seen
 	var index = -1;
 	for (i = 0; i < len; ++i) {
-		//console.log("Sort: " + list_as_array[i].senttime);
-		//		if  (new Date(list_as_array.message[i].senttime) <= new Date(date_stamp)) {
 		if (new Date(list_as_array[i].senttime) <= new Date(date_stamp)) {
 			index = i;
 			break;
@@ -217,15 +250,11 @@ function scan_list_for_new_messages(list, date_stamp, feed) {
 		}
 	}
 
-	//console.log(index);
 	if (index > 0) {
 		// index points to the last already seen element + 1
 		for (i = index; i-- > 0;) {
-			//			if (i in list.message) {
-			//				s = list.message[i];
 			if (i in list_as_array) {
 				s = list_as_array[i];
-				//console.log(s.channel + " - " + s.senttime + " - " + s.sender + ": " + s.text);
 				if (new Date(s.senttime) > new Date(date_stamp)) {
 					sendItem(s, feed);
 					logger.info("Send: " + s.channel + " - " + s.type + " / " + s.senttime + " - " + s.sender + ": " + s.text);
@@ -254,7 +283,7 @@ function sendItem(item, feed) {
 		// }
 		if (feed.discord !== undefined) {
 			if (feed.enabled) {
-				send_to_discord(item.text, item.player, item.sender, item.senttime, item.server, feed.discord.channelID, feed.discord.color);
+				send_to_discord(item.text, item.player, item.sender, item.senttime, item.server, feed.discord.channelIDs, feed.discord.color, feed.name === "run");
 			}
 		}
 	}
@@ -266,7 +295,7 @@ function readStats(callback) {
 			Body: data
 		};
 		if (err) {
-			console.log("Error reading stats.json:" + jsonStringify(err));
+			logger.error("Error reading stats.json:" + jsonStringify(err));
 		}
 		else {
 			var storedPrefs = JSON.parse(dataAboutRead.Body);
@@ -286,23 +315,20 @@ function readStats(callback) {
 function writeStats() {
 	fs.writeFile(stat_file, jsonStringify(appStats), function (err) {
 		if (err) {
-			return console.log(err);
+			return logger.error(err);
 		}
-		//console.log("Debug output: File saved");
 	});
 	//fs.writeFile ("stats.json", jsonStringify (appStats));
 }
 
 
 function readConfig(callback) {
-	// Using temporay an absolute path
-	//fs.readFile ("config.json", "utf8", function (err, data) {
 	fs.readFile(config_file, "utf8", function (err, data) {
 		var dataAboutRead = {
 			Body: data
 		};
 		if (err) {
-			console.log("readConfig: error == " + jsonStringify(err));
+			logger.error("readConfig: error == " + jsonStringify(err));
 		}
 		else {
 			appConfig = JSON.parse(dataAboutRead.Body);
@@ -329,49 +355,56 @@ function everySecond() {
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-	// Send the message to a designated channel on a server:
-	const channel = member.guild.channels.find('name', 'member-log');
-	// Do nothing if the channel wasn't found on this server
-	if (!channel) return;
-	// Send the message, mentioning the member
-	channel.send(`Welcome to the server, ${member}`);
+
+	// // Send the message to a designated channel on a server:
+	// const channel = member.guild.channels.find('name', 'member-log');
+	// // Do nothing if the channel wasn't found on this server
+	// if (!channel) return;
+	// // Send the message, mentioning the member
+	// channel.send(`Welcome to the server, ${member}`);
+
 });
 
 client.on("guildCreate", guild => {
 	// This event triggers when the bot joins a guild.
 	logger.info(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-	client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {
 	// this event triggers when the bot is removed from a guild.
 	logger.info(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-	client.user.setGame(`on ${client.guilds.size} servers`);
 });
 
 function logMapElements(value, key, map) {
-	console.log(`m[${key}] = ${value}`);
+	logger.debug(`m[${key}] = ${value}`);
 }
 
 client.on("ready", () => {
-	client.emojis.cache.forEach(logMapElements);
-	// send_to_discord("Starting service", "Bot status message", "bot", new Date().toISOString().split('.')[0] + "Z");
+	// client.emojis.cache.forEach(logMapElements);
+
+	// send_to_discord("Starting service", "Bot status message", "bot", new Date().toISOString().split('.')[0] + "Z", 311, "426339936044843018", "#6087FF", false);
+	// send_to_discord("Starting service", "Bot status message", "bot", new Date().toISOString().split('.')[0] + "Z", 311, "760230386223022120", "#6087FF", false);
+	// send_to_discord("Starting service", "Bot status message", "HG-Announcer", new Date().toISOString().split('.')[0] + "Z", 311, ["426339936044843018", "760230386223022120"], "#6087FF", false);
 
 	logger.info('Connected');
-	logger.info('Logged in as: ');
-	logger.info(client.username + ' - (' + client.id + ')');
+	logger.info(`Hosting ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
+
+	// client.user.setPresence({ activity: { name: `${client.guilds.cache.size} server`, type: 'WATCHING' }, status: 'online' })
+	// 	.then(logger.info)
+	// 	.catch(logger.error);
 });
 
 client.login(token);
 
-process.on('unhandledRejection', error => console.error(`Uncaught Promise Rejection:\n${error}`));
+process.on('unhandledRejection', error => logger.error(`Uncaught Promise Rejection:\n${error}`));
+
+parser.on('error', function (err) { logger.error('Parser error', err); });
 
 readConfig(function () {
 	readStats(function () {
 		logger.info("\n" + ProjectName + " v" + Version + ".");
 		refresh_xml();
 		setInterval(refresh_xml, interval);
-		//		setInterval (refresh_xml, 5000);
 		setInterval(everySecond, 1000);
 	});
 });
